e1f932d4987e2376c667c6220e42a399b4a39233
alejandro <alexgeorgegeer@gmail.com>
2024-01-27 00:00:52 -0600

    Implemented validation of user input and debugged said implementation

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const inquirer = require('inquirer');
+
+// Utilizing an NPM package to validate user input of color fields
+// The package includes methods to check strings and validate whether they are valid HTML colors or hexadecimal color values
+// Credit to Wallace Sidhree for the NPM package. https://github.com/dreamyguy/validate-color
+const {validateHTMLColorName, validateHTMLColorHex} = require('validate-color');
+
 const fs = require("fs");
 const { Triangle, Square, Circle } = require('./lib/shapes.js')
 
@@ -29,34 +35,26 @@ const prompts = [
 function init() {
   const logoData = inquirer.prompt(prompts)
   .then(data => {
-    // Validate User Input
-    if (data.initials.length > 3 || data.initials.length < 1 ) {
-      console.log('Invalid number of initials. Try again.')
-    };
-    // Return data for processing
-    return data;
+    // Validate User Input: 1 - 3 initials, and valid color values:
+    if ( !initsOK(data.initials) ) {
+      throw new Error('Invalid number of initials. Try again.')
+    } else if ( !isColor(data.fontColor) || !isColor(data.bgColor)) {
+      throw new Error('You must enter valid color properties');
+    } else if ( initsOK(data.initials) && isColor(data.fontColor) && isColor(data.bgColor) ) {
+      return data;
+    } else {
+      throw new Error("Something isn't right here. But it's not you - it's me.")
+    }
+  
   })
   .then( data => {
     // Get the bespoke text of the SVG file 
-    let logo;
-    switch (data.shape) {
-      case "Triangle":
-        logo = new Triangle(data.initials, data.fontColor, data.bgColor)
-        break;
-      case "Square":
-        logo = new Square(data.initials, data.fontColor, data.bgColor)
-        break;
-      case "Circle":
-        logo = new Circle(data.initials, data.fontColor, data.bgColor)
-        break;
-    
-      default: console.log('Error: no shape');
-        break;
-    }
-    return logo;
+    const logoCode = getLogoCode(data);
+    return logoCode;
 
   })
   .then( logo => {
+    // Now write it to a file...
     const svgText = logo.generateSVG();
     fs.writeFile(
       `examples/logo${logo.initials}.svg`,
@@ -65,12 +63,45 @@ function init() {
         if (err) return console.log(err);
       }
     )
+  })
+  .catch(error => {
+    console.log(error.message);
+    init();
   });
 }
 
+// HELPER FUNCTIONS
 
-// const triangle = new Triangle('CGT', 'Blue', 'White');
-// console.log(triangle)
-// console.log(triangle.generateSVG());
+// Generate a bespoke SVG, calling methods from methods required in from "lib/shapes"
+function getLogoCode(data) {
+  let logo;
+  switch (data.shape) {
+    case "Triangle":
+      logo = new Triangle(data.initials, data.fontColor, data.bgColor)
+      break;
+    case "Square":
+      logo = new Square(data.initials, data.fontColor, data.bgColor)
+      break;
+    case "Circle":
+      logo = new Circle(data.initials, data.fontColor, data.bgColor)
+      break;
+  
+    default: console.log('Error: no shape');
+      break;
+  }
+  return logo;
+}
 
+// Verify initials is up to 3 characters
+function initsOK ( str ) {
+  return str.length > 0 && str.length < 4;
+}
+
+// Verify colors are valid CSS values 
+// Credit to Wallace Sidhree for the NPM package. https://github.com/dreamyguy/validate-color
+function isColor( color ) {
+  return validateHTMLColorHex(color) || validateHTMLColorName(color);
+}
+
+// Let's get started...
 init();
