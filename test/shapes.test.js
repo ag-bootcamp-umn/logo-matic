28cfe1943cd041d1275f5ee13f1ffc1b585f9d15
alejandro <alexgeorgegeer@gmail.com>
2024-02-05 21:15:02 -0600

    Added colors to test

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -5,8 +5,8 @@ const { Triangle, Square, Circle } = require('../lib/shapes.js');
 describe('Shape', () => {
   describe('Triangle', () => {
     it('Should return the shape of "polygon"', () => {
-      const shape = new Triangle()
-      expect(shape.shape.split(' ')[0]).toEqual('polygon');
+      const shape = new Triangle('BFG', 'red', 'green')
+      expect(shape.shape.split(' ')[0]).toEqual('polygon')
     });
   });
 }) ;
@@ -27,4 +27,22 @@ describe('Shape', () => {
       expect(shape.shape.split(' ')[0]).toEqual('circle');
     });
   });
-}) ;
+});
+
+describe('Shape', () => {
+  describe('Background Color', () => {
+    it('Should return the background color "#d9de6e"', () => {
+      const shape = new Circle('BFG', '#381a6b', '#d9de6e');
+      expect(shape.bgColor).toEqual('#d9de6e');
+    });
+  });
+});
+
+describe('Shape', () => {
+  describe('Font Color', () => {
+    it('Should return the font color "#381a6b"', () => {
+      const shape = new Circle('BFG', '#381a6b', '#d9de6e');
+      expect(shape.fontColor).toEqual('#381a6b');
+    });
+  });
+});
